14a3de8f886c17a27d15a937c3ed24767736ee83
jeff@gsat.us <jeff@gsat.us>
2021-01-24 01:43:30 +0000

    Add option for configuring gluster-kubernetes project image deployment

--- scripts/job/05-deploy-gluster-and-heketi.sh
+++ scripts/job/05-deploy-gluster-and-heketi.sh
@@ -5,5 +5,5 @@ deploy_path="$GLUSTER_HEKETI_BOOTSTRAP_DIR/gluster-kubernetes/deploy"
 # ---------------- [START] Deploy GlusterFS DaemonSet, Heketi ---------------- #
 cd "$deploy_path"
 
-gk-deploy -gvy --admin-key $HEKETI_KEYADMIN --user-key $HEKETI_KEYUSER
+gk-deploy -gvy --admin-key $HEKETI_KEYADMIN --user-key $HEKETI_KEYUSER --gluster-image-name $GLUSTER_IMAGE_NAME
 # ----------------- [END] Deploy GlusterFS DaemonSet, Heketi ----------------- #
